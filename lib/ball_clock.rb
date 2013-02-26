d30bfdf525a48d60123974051172e0b4c6b47f8a
Tad Thorley <phaedryx@gmail.com>
2013-02-26 13:22:42 -0700

    create a tick method that takes a clock state and returns the next clock state

--- lib/ball_clock.rb
+++ lib/ball_clock.rb
@@ -2,7 +2,6 @@ class BallClock
   class ArgumentError < StandardError; end
 
   def initialize()
-    @ball_counts = []
   end
 
   def check_count!(count)
@@ -11,5 +10,17 @@ class BallClock
 
   def ball_cycle_length_for_count(count)
     check_count!(count)
+    queue = Array.new(count) {|i| i+1 }
+  end
+
+  def tick(params={})
+    queue             = params[:queue]
+    minute_track      = params[:minute_track]
+    five_minute_track = params[:five_minute_track]
+    hour_track        = params[:hour_track]
+    minutes           = params[:minutes]
+
+    minutes += 1
+    {queue: queue, minute_track: minute_track, five_minute_track: five_minute_track, hour_track: hour_track, minutes: minutes}
   end
 end
--- spec/ball_clock_spec.rb
+++ spec/ball_clock_spec.rb
@@ -12,4 +12,14 @@ describe BallClock do
       lambda { BallClock.new.ball_cycle_length_for_count(128) }.must_raise BallClock::ArgumentError
     end
   end
+
+  describe "#tick" do
+    it "must increase minutes by 1 for each tick" do
+      clock = BallClock.new
+      queue = Array.new(27) {|i| i+1 }
+      minutes = 0
+      results = clock.tick({queue: queue, minute_track: [], five_minute_track: [], hour_track: [], minutes: minutes})
+      results[:minutes].must_equal minutes + 1
+    end
+  end
 end
