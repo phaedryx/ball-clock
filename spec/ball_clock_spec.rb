30a2697c5e707ceb793acd43c3ee65ed32dacf1d
Tad Thorley <phaedryx@gmail.com>
2013-02-26 14:58:30 -0700

    got the rules working in the tick method, tests pass

--- spec/ball_clock_spec.rb
+++ spec/ball_clock_spec.rb
@@ -21,5 +21,23 @@ describe BallClock do
       results = clock.tick({queue: queue, minute_track: [], five_minute_track: [], hour_track: [], minutes: minutes})
       results[:minutes].must_equal minutes + 1
     end
+
+    it "must add the first queue ball to the minute track if it isn't full" do
+      clock = BallClock.new
+      queue = Array.new(27) {|i| i+1 }
+      minutes = 0
+      results = clock.tick({queue: queue, minute_track: [], five_minute_track: [], hour_track: [], minutes: minutes})
+      results[:minute_track].must_equal [1]
+    end
+
+    it "must empty the minute track into the queue and add the ball to the five minute track if the minute track is full" do
+      clock = BallClock.new
+      queue = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+      minute_track = [1, 2, 3, 4]
+      minutes = 0
+      results = clock.tick({queue: queue, minute_track: minute_track, five_minute_track: [], hour_track: [], minutes: minutes})
+      results[:minute_track].must_be_empty
+      results[:five_minute_track].must_equal [5]
+    end
   end
 end
