e323e5616b1b9edae9b40e106b1a663650658e2f
Tad Thorley <phaedryx@gmail.com>
2013-02-26 12:52:55 -0700

    simplify things: don't make the ball clock class responsible for a file

--- lib/ball_clock.rb
+++ lib/ball_clock.rb
@@ -1,19 +1,15 @@
 class BallClock
-  class FileError < StandardError; end
   class ArgumentError < StandardError; end
 
-  def initialize(file)
-    raise FileError, "expected File, received #{file.class} instead" unless file.is_a?(File)
+  def initialize()
     @ball_counts = []
-    file.each_line do |count|
-      count = count.strip.to_i
-      break if count.to_i == 0
-      check_count!(count)
-      @ball_counts << count
-    end
   end
 
   def check_count!(count)
     raise ArgumentError, "ball count must be between 27 and 127" unless count >= 27 && count <= 127
+  end
+
+  def ball_cycle_length_for_count(count)
+    check_count!(count)
   end
 end
--- spec/ball_clock_spec.rb
+++ spec/ball_clock_spec.rb
@@ -3,21 +3,13 @@ require 'minitest/autorun'
 require 'ball_clock'
 
 describe BallClock do
-  describe "#initialize" do
-    it "must raise an error when not provided with a file" do
-      -> { BallClock.new("not a file") }.must_raise BallClock::FileError
-    end
-  end
-
   describe "#check_count!" do
     it "must raise an error when the ball count is too low" do
-      file = File.open("#{Dir.pwd}/spec/test_data/too_low.txt")
-      -> { BallClock.new(file) }.must_raise BallClock::ArgumentError
+      -> { BallClock.new.ball_cycle_length_for_count(26) }.must_raise BallClock::ArgumentError
     end
 
     it "must raise an error when the ball count is to high" do
-      file = File.open("#{Dir.pwd}/spec/test_data/too_high.txt")
-      -> { BallClock.new(file) }.must_raise BallClock::ArgumentError
+      -> { BallClock.new.ball_cycle_length_for_count(128) }.must_raise BallClock::ArgumentError
     end
   end
 end
