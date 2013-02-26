aa93e6bc53c367265ecd435ff89b9dfe99a340ec
Tad Thorley <phaedryx@gmail.com>
2013-02-26 13:09:22 -0700

    use lambda keyword instead of stabby lambda for clarity

--- spec/ball_clock_spec.rb
+++ spec/ball_clock_spec.rb
@@ -5,11 +5,11 @@ require 'ball_clock'
 describe BallClock do
   describe "#check_count!" do
     it "must raise an error when the ball count is too low" do
-      -> { BallClock.new.ball_cycle_length_for_count(26) }.must_raise BallClock::ArgumentError
+      lambda { BallClock.new.ball_cycle_length_for_count(26) }.must_raise BallClock::ArgumentError
     end
 
     it "must raise an error when the ball count is to high" do
-      -> { BallClock.new.ball_cycle_length_for_count(128) }.must_raise BallClock::ArgumentError
+      lambda { BallClock.new.ball_cycle_length_for_count(128) }.must_raise BallClock::ArgumentError
     end
   end
 end
