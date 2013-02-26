4c8171556912e3cee8edd6739b9f5b3c219998f0
Tad Thorley <phaedryx@gmail.com>
2013-02-26 11:15:53 -0700

    ensure that the ball count is between 27 and 127

--- lib/ball_clock.rb
+++ lib/ball_clock.rb
@@ -1,8 +1,22 @@
 class BallClock
   class FileError < StandardError; end
+  class ArgumentError < StandardError; end
 
   def initialize(file)
     raise FileError, "expected File, received #{file.class} instead" unless file.is_a?(File)
 
+    file.each_line do |count|
+      count = count.strip.to_i
+      break if count.to_i == 0
+      display_days_for_balls(count)
+    end
+  end
+
+  def check_count!(count)
+    raise ArgumentError, "ball count must be between 27 and 127" unless count >= 27 && count <= 127
+  end
+
+  def display_days_for_balls(count)
+    check_count!(count)
   end
 end
--- spec/ball_clock_spec.rb
+++ spec/ball_clock_spec.rb
@@ -8,4 +8,16 @@ describe BallClock do
       -> { BallClock.new("not a file") }.must_raise BallClock::FileError
     end
   end
+
+  describe "#check_count!" do
+    it "must raise an error when the ball count is too low" do
+      file = File.open("#{Dir.pwd}/spec/test_data/too_low.txt")
+      -> { BallClock.new(file) }.must_raise BallClock::ArgumentError
+    end
+
+    it "must raise an error when the ball count is to high" do
+      file = File.open("#{Dir.pwd}/spec/test_data/too_high.txt")
+      -> { BallClock.new(file) }.must_raise BallClock::ArgumentError
+    end
+  end
 end
