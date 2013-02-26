5ac2b9201910c4b18e9bc6def6d30a07b423ad92
Tad Thorley <phaedryx@gmail.com>
2013-02-26 12:32:58 -0700

    refactor tracking of ball counts

--- lib/ball_clock.rb
+++ lib/ball_clock.rb
@@ -4,19 +4,16 @@ class BallClock
 
   def initialize(file)
     raise FileError, "expected File, received #{file.class} instead" unless file.is_a?(File)
-
+    @ball_counts = []
     file.each_line do |count|
       count = count.strip.to_i
       break if count.to_i == 0
-      display_days_for_balls(count)
+      check_count!(count)
+      @ball_counts << count
     end
   end
 
   def check_count!(count)
     raise ArgumentError, "ball count must be between 27 and 127" unless count >= 27 && count <= 127
-  end
-
-  def display_days_for_balls(count)
-    check_count!(count)
   end
 end
