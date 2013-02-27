130480a897e47e6bb9d324a63033f53976256425
Tad Thorley <phaedryx@gmail.com>
2013-02-26 19:23:29 -0700

    move the tick method into ball_cycle_length_for_count

--- lib/ball_clock.rb
+++ lib/ball_clock.rb
@@ -10,38 +10,39 @@ class BallClock
 
   def ball_cycle_length_for_count(count)
     check_count!(count)
-    queue = Array.new(count) {|i| i+1 }
-  end
+    queue             = Array.new(count) {|i| i+1 }
+    orig_queue        = queue.dup # make a comparison queue
+    # set everything to an initial state
+    minutes           = 0
+    minute_track      = []
+    five_minute_track = []
+    hour_track        = []
 
-  def tick(params={}) # takes a current clock state and returns the next clock state
-    queue             = params[:queue]
-    minute_track      = params[:minute_track]
-    five_minute_track = params[:five_minute_track]
-    hour_track        = params[:hour_track]
-    minutes           = params[:minutes]
-
-    # take the first ball from the queue
-    curr_ball = queue.slice!(0)
-    if minute_track.size < 4
-      minute_track << curr_ball              # add the ball to the minute track if there is room
-    else                                     # it continues if there isn't room
-      queue += minute_track.reverse          # add the minute track back to the queue (reversed)
-      minute_track.clear                     # empty the minute track
-      if five_minute_track.size < 11
-        five_minute_track << curr_ball       # add the ball to the five minute track if there is room
-      else                                   # it continues if there isn't room
-        queue += five_minute_track.reverse   # add the five minute track to the queue (reversed)
-        five_minute_track.clear              # empty the five minute track
-        if hour_track.size < 11
-          hour_track << curr_ball            # add the ball to the hour track if there is room
-        else                                 # it continues if there isn't room
-          queue += hour_track.reverse        # add the hour track to the queue (reversed)
-          hour_track.clear                   # empty the hour track
-          queue << curr_ball                 # remember to add the current ball to the end of the queue
+    loop do
+      # take the first ball from the queue
+      curr_ball = queue.slice!(0)
+      if minute_track.size < 4
+        minute_track << curr_ball              # add the ball to the minute track if there is room
+      else                                     # it continues if there isn't room
+        queue += minute_track.reverse          # add the minute track back to the queue (reversed)
+        minute_track.clear                     # empty the minute track
+        if five_minute_track.size < 11
+          five_minute_track << curr_ball       # add the ball to the five minute track if there is room
+        else                                   # it continues if there isn't room
+          queue += five_minute_track.reverse   # add the five minute track to the queue (reversed)
+          five_minute_track.clear              # empty the five minute track
+          if hour_track.size < 11
+            hour_track << curr_ball            # add the ball to the hour track if there is room
+          else                                 # it continues if there isn't room
+            queue += hour_track.reverse        # add the hour track to the queue (reversed)
+            hour_track.clear                   # empty the hour track
+            queue << curr_ball                 # remember to add the current ball to the end of the queue
+          end
         end
       end
+      minutes += 1
+      break if queue == orig_queue
     end
-    minutes += 1
-    {queue: queue, minute_track: minute_track, five_minute_track: five_minute_track, hour_track: hour_track, minutes: minutes}
+    minutes / (24*60) # convert minutes to days
   end
 end
